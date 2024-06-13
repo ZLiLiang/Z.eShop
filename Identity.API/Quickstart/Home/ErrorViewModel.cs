60d94e945b919c9e75fae7d13ceff54f8b85efd2
LL zhang <zhang31666@outlook.com>
2024-06-13 14:41:10 +0800

    新增主页控制器

---
+++ Identity.API/Quickstart/Home/ErrorViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace IdentityServerHost.Quickstart.UI
+{
+    public class ErrorViewModel
+    {
+        public ErrorViewModel()
+        {
+        }
+
+        public ErrorViewModel(string error)
+        {
+            Error = new ErrorMessage { Error = error };
+        }
+
+        public ErrorMessage Error { get; set; }
+    }
+}
