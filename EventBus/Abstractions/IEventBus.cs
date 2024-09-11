de023127e14dfae062e61dcb09f22804c79c9ba0
LL zhang <zhang31666@outlook.com>
2024-09-11 20:57:01 +0800

    添加事件总线模块

---
+++ EventBus/Abstractions/IEventBus.cs
@@ -0,0 +1,6 @@
+﻿namespace Z.eShop.EventBus.Abstractions;
+
+public interface IEventBus
+{
+    Task PublishAsync(IntegrationEvent @event);
+}
---
+++ EventBus/Abstractions/IEventBusBuilder.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Z.eShop.EventBus.Abstractions;
+
+public interface IEventBusBuilder
+{
+    public IServiceCollection Services { get; }
+}
